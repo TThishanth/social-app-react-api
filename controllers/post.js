98594d07f369cca77a877dcadcd8241090098719
TThishanth <thishanth41@gmail.com>
2023-08-06 12:25:14 +0530

    Final commit

--- controllers/post.js
+++ controllers/post.js
@@ -12,13 +12,15 @@ export const getPosts = (req, res) => {
 
         if(err) return res.status(403).json("Token invalid!");
 
-        const q = userId 
+        console.log(userId);
+
+        const q = userId !== "undefined"
         ? `SELECT p.*, u.id AS userId, name, profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId) WHERE p.userId = ?` 
         : `SELECT p.*, u.id AS userId, name, profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId)
         LEFT JOIN relationships AS r ON (p.userId = r.followedUserId) WHERE r.followerUserId = ? OR p.userId = ?
         ORDER BY p.createdAt DESC`;
 
-        const values = userId ? [userId] : [userInfo.id, userInfo.id];
+        const values = userId !== "undefined" ? [userId] : [userInfo.id, userInfo.id];
     
         db.query(q, values, (err, data) => {
     
@@ -53,6 +55,32 @@ export const addPost = (req, res) => {
     
             if(err) return res.status(500).json(err);
             return res.status(200).json("Post has been created!");
+            
+        });
+
+    });
+    
+}
+
+export const deletePost = (req, res) => {
+    
+    const token = req.cookies.accessToken;
+    if(!token) return res.status(401).json("User not logged in!");
+
+    jwt.verify(token, "secretkey", (err, userInfo) => {
+
+        if(err) return res.status(403).json("Token invalid!");
+
+        const q = "DELETE FROM posts WHERE `userId` = ? AND `id` = ?";
+    
+        db.query(q, [ userInfo.id, req.params.id ], (err, data) => {
+    
+            if(err) return res.status(500).json(err);
+            if (data.affectedRows > 0) {
+                return res.status(200).json("Post has been removed!");
+            } else {
+                return res.status(403).json("You can't delete this post!");
+            }
             
         });
 
